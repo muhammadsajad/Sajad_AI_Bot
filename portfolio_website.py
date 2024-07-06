dfbb72d883b1f783adbe6d380dcd3f7b891be8dc
Muhammad Sajad <muhammad_sajad47@yahoo.com>
2024-07-06 12:30:04 +0500

    Update portfolio_website.py

--- portfolio_website.py
+++ portfolio_website.py
@@ -6,7 +6,7 @@ genai.configure(api_key=api_key)
 
 
 
-model = genai.GenerativeModel('gemini-1.5-Pro')
+model = genai.GenerativeModel('gemini-1.5-pro')
 
 # col1, col2 =st.columns(2)
 col1, col2 = st.columns([3, 1])  # Increase the width of the second column
